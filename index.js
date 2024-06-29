58abadbb47a716c97cb6c977c1895e168a0cf8aa
henrique062 <visioneplays@gmail.com>
2024-06-29 10:08:21 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import { OpenSeaStreamClient, Network } from '@opensea/stream-js';
+import { OpenSeaStreamClient } from '@opensea/stream-js';
 import { WebSocket } from 'ws';
 import { LocalStorage } from 'node-localstorage';
-require('dotenv').config();
+import 'dotenv/config';
 
 const client = new OpenSeaStreamClient({
-  token: process.env.OPENSEA_API_KEY, 
+  token: process.env.OPENSEA_API_KEY,
   connectOptions: {
     transport: WebSocket,
     sessionStorage: LocalStorage
   }
 });
 
-client.onItemListed('sunflower-land-collectibles', (event) => {
-  console.log(event); 
+client.onItemListed('collection-slug', (event) => {
+  console.log(event);
 });
 
-client.onItemSold('sunflower-land-collectibles', (event) => {
+client.onItemSold('collection-slug', (event) => {
   console.log(event);
 });
 
